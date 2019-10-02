699d54f0f72bb3790edf009a066208cb1171c338
Steve Farrell <sparticlesteve@gmail.com>
2019-10-01 22:19:49 -0700

    fixed get_weight_norm

--- utils/checks.py
+++ utils/checks.py
@@ -2,5 +2,5 @@ def get_weight_norm(model, norm_type=2):
     """Get the norm of the model weights"""
     norm = 0
     for p in model.parameters():
-        norm += p.norm(norm_type).item()
-    return norm
+        norm += p.data.norm(norm_type).item() ** norm_type
+    return norm ** (1. / norm_type)
