6eac1135ede5d0dfbdf4f9c1f1489dc14498e264
Steve Farrell <sparticlesteve@gmail.com>
2019-10-01 22:20:22 -0700

    add special utility function for computing grad norm

--- utils/checks.py
+++ utils/checks.py
@@ -4,3 +4,10 @@ def get_weight_norm(model, norm_type=2):
     for p in model.parameters():
         norm += p.data.norm(norm_type).item() ** norm_type
     return norm ** (1. / norm_type)
+
+def get_grad_norm(model, norm_type=2):
+    """Get the norm of the model weight gradients"""
+    norm = 0
+    for p in model.parameters():
+        norm += p.grad.data.norm(norm_type).item() ** norm_type
+    return norm ** (1. / norm_type)
